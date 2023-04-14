88d1d1df750d408aacfe436c8427d2b2782ed395
Internships <84063946+vin-stack@users.noreply.github.com>
2023-04-14 19:28:15 +0530

    Update App.py

--- App.py
+++ App.py
@@ -36,7 +36,7 @@ def run():
     if img_file is not None:
         img = Image.open(img_file).resize((250, 250))
         st.image(img, use_column_width=False)
-        save_image_path = './upload_images/' + img_file.name
+        save_image_path = '/tmp' + img_file.name
         with open(save_image_path, "wb") as f:
             f.write(img_file.getbuffer())
 
